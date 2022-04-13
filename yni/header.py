b3f79758b05b72e88cfec9df1ec01bca25fa3349
Zeno <57388886+ZenoABC@users.noreply.github.com>
2022-04-13 10:28:41 +0300

    Update header.py

--- yni/header.py
+++ yni/header.py
@@ -1,17 +1,29 @@
-from typing import Any, Dict, List, Optional, Tuple, Generic, TypeVar
-
+from typing import Any, Dict, List
 from dataclasses import dataclass, field
 
 
 @dataclass
 class Header:
     name: str
-    attributes: Dict[Any, Any] = field(default_factory=dict)
-    children = field(default_factory=list)
+    attributes: Dict[str, Any] = field(default_factory=dict)
+    children: "List[Child]" = field(default_factory=list)
+
+    def __getitem__(self, name: str):
+        return self.attributes[name]
+    def __setitem__(self, name: str, value: Any):
+        self.attributes[name] = value
+
+    def __getattribute__(self, __name: str) -> Any: # WIP
+        pass
 
 
 @dataclass
 class Child:
     parent: Header
     name: str
-    attributes: Dict[Any, Any] = field(default_factory=dict)
+    attributes: Dict[str, Any] = field(default_factory=dict)
+
+    def __getitem__(self, name: str):
+        return self.attributes[name]
+    def __setitem__(self, name: str, value: Any):
+        self.attributes[name] = value
