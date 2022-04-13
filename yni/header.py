4351245576df8f2f6a0beeec3a4be41f3dd521a2
35807505 <57388886+35807505@users.noreply.github.com>
2022-04-13 10:44:29 +0300

    try to allow thing.thing and thing["thing"]

--- yni/header.py
+++ yni/header.py
@@ -2,28 +2,33 @@ from typing import Any, Dict, List
 from dataclasses import dataclass, field
 
 
+class BaseClass:
+    attributes: Dict[str, Any]
+
+    def __getitem__(self, name: str):
+        return self.attributes[name]
+    def __setitem__(self, name: str, value: Any):
+        self.attributes[name] = value
+
+    def __getattribute__(self, __name: str) -> Any:
+        if __name in super().__getattribute__("attributes"):
+            return super().__getattribute__("attributes")[__name]
+        return super().__getattribute__(__name)
+
+    def __setattr__(self, __name: str, __value: Any) -> None: # this one dies
+        if __name in super().__getattribute__("attributes"):
+            super().__getattribute__("attributes")[__name] = __value
+            return
+        return super().__setattr__(__name, __value)
+
 @dataclass
-class Header:
+class Header(BaseClass):
     name: str
     attributes: Dict[str, Any] = field(default_factory=dict)
     children: "List[Child]" = field(default_factory=list)
 
-    def __getitem__(self, name: str):
-        return self.attributes[name]
-    def __setitem__(self, name: str, value: Any):
-        self.attributes[name] = value
-
-    def __getattribute__(self, __name: str) -> Any: # WIP
-        pass
-
-
 @dataclass
-class Child:
+class Child(BaseClass):
     parent: Header
     name: str
     attributes: Dict[str, Any] = field(default_factory=dict)
-
-    def __getitem__(self, name: str):
-        return self.attributes[name]
-    def __setitem__(self, name: str, value: Any):
-        self.attributes[name] = value
